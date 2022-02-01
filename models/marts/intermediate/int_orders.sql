add3e1885e31e826647d4f6cef2e6319aa4f2d6c
matthew.anttila@umbra.com <matthew.anttila@umbra.com>
2022-02-01 19:08:20 +0000

    test

---
+++ models/marts/intermediate/int_orders.sql
@@ -0,0 +1,41 @@
+orders as (
+
+  select * from {{ ref('stg_jaffle_shop__orders') }}
+
+),
+
+payments as (
+
+  select * from {{ ref('stg_stripe__payments') }}
+  where payments.payment_status != 'fail'
+
+),
+
+order_totals as (
+
+  select
+
+    order_id,
+    payment_status,
+    sum(payment_amount) as order_value_dollars
+
+  from payments
+  group by 1, 2
+
+),
+
+order_values_joined as (
+
+  select
+
+  orders.*,
+  orders_totals.payment_status,
+  orders_totals.order_value_dollars
+
+  from orders
+  left join order_totals
+    on orders.order_id = order_totals.order_id
+
+)
+
+select * from order_values_joined
